7eb86b113a249ce0cc0e66dd53f3eed90bafb3f4
Camila Ester <126359118+niinofonseca@users.noreply.github.com>
2025-06-07 17:28:29 -0300

    Produto.java

--- src/dominio/Produto.java
+++ src/dominio/Produto.java
@@ -1,4 +1,20 @@
 package dominio;
 
 public class Produto {
+    public String nome;
+    String tipo;
+    double preco;
+
+    //construtor
+    public Produto(String nome, String tipo, double preco) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    // Método para exibir os dados do produto
+    @Override
+    public String toString() {
+        return "Nome: " + nome + " | Tipo: " + tipo + " | Preço: R$ " + preco;
+    }
 }
